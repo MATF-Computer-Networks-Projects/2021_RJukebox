012da74f1a220ef305e65720319a6b262e8341e9
SpotRZ <spot.rusherz96@gmail.com>
2021-05-06 21:47:40 +0200

    implementation for patch

--- api/api_patch.py
+++ api/api_patch.py
@@ -3,28 +3,60 @@ import logging
 from flask import Blueprint
 from flask import make_response
 from flask import jsonify
+from flask import request
 
 from utilities.db_util import connect_execute_query
+from db_templates.load_template import load_db_template
+import defaults
 
 
 api_patch = Blueprint('api_patch', __name__)
 
 
-@api_getter.route('/api/songs/name', methods=['PATCH'])
+@api_patch.route('/api/songs/name', methods=['PATCH'])
 def change_song_name():
-    return make_response("Song name successfully updated", 200)
+    return _handle_patch_request('song_name')
 
 
-@api_getter.route('/api/songs/artist', methods=['PATCH'])
+@api_patch.route('/api/songs/artist', methods=['PATCH'])
 def change_artist():
-    return make_response("Artist successfully updated", 200)
+    return _handle_patch_request('artist')
 
 
-@api_getter.route('/api/songs/genre', methods=['PATCH'])
+@api_patch.route('/api/songs/genre', methods=['PATCH'])
 def change_genre():
-    return make_response("Genre successfully updated", 200)
+    return _handle_patch_request('genre')
 
 
-@api_getter.route('/api/songs/link', methods=['PATCH'])
+@api_patch.route('/api/songs/link', methods=['PATCH'])
 def change_yt_link():
-    return make_response("Youtube link successfully updated", 200)
+    return _handle_patch_request('yt_link')
+
+
+def _handle_patch_request(field: str):
+    try:
+        logging.info(f"User requested [{field}] change")
+
+        if not _required_params():
+            return make_response("Invalid parameter(s)", 406)
+
+        update_value = request.args.get('update')
+        song_name = request.args.get('song_name')
+        artist = request.args.get('artist')
+
+        template = load_db_template(defaults.update_songs_column)
+        query = template.render(field=field, update_value=update_value, song_name=song_name, artist=artist)
+
+        connect_execute_query(query)
+
+        logging.info(f"[{field}] successfully changed to {update_value}.")
+
+        return make_response(f"Successfully changed [{field}] field.", 200)
+    except Exception as e:
+        logging.error(f"Unexpected error happened: {e}")
+        return make_response("Unprocessable entity", 430)
+
+
+def _required_params() -> bool:
+    valid_params = [ 'song_name', 'artist', 'update' ]
+    return set(valid_params) == set([arg for arg in request.args])
