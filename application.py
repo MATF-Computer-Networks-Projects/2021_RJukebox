9df1dbe963265749c0446bf7011d557bc67b10c4
somislav <milos.nik102@gmail.com>
2021-05-09 13:06:53 +0200

    Login commit

--- application.py
+++ application.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template,flash,request
-
+from flask import Flask, render_template,flash,request,redirect,url_for # izbrisi suvisni svi sem flask
 import os
 import defaults
-
 from dotenv import load_dotenv
-
-import logging
+from werkzeug.security import generate_password_hash, check_password_hash #importuj iz util
+import logging 
 from utilities.logger_utilities import setup_logger
-
+from classes.User import User #prebaci
 from test_db.create_db import setup_db
-
+from flask_login import login_user, login_required, logout_user, current_user # cela linija
 load_dotenv('.env')
 setup_logger(defaults.logger_config)
 
@@ -28,24 +26,45 @@ with app.app_context():
     app.register_blueprint(api_post.api_post)
     app.register_blueprint(api_delete.api_delete)
     app.register_blueprint(api_patch.api_patch)
-    app.config['SECRET_KEY']='asdasdasdasdasdasd'
-    @app.route('/', methods=['GET', 'POST'])
+    app.config['SECRET_KEY']='asdasdasdasdasdasd'#dot env
+    @app.route('/',)  
     def index():
         return render_template("home.html")
-    @app.route('/login',methods=['GET', 'POST'])
+    @app.route('/login',methods=['GET', 'POST']) # brisi ifove samo post
     def login():
-        
+        if request.method =='POST':
+            user=request.form.get('user')
+            password=request.form.get('password')
+            user=User.query.filter_by(user=user).first()
+            if user:
+                if check_password_hash(user.password,password):
+                    flash('Logged in successfully!', category='success')
+                    login_user(user,remember=True)
+                    return redirect(url_for('views.main'))
+                else:
+                    flash('Incorrect password, try again', category='error')
+            else:
+                flash('Email does not exist',category='error')        
         return render_template("login.html")
     @app.route('/logout')
+    @login_required
     def logout():
-        return "<p>logout</p>"
+        return redirect(url_for('auth.login'))
+    @app.route('/main')
+    def main():
+        return render_template('main.html')
+
     @app.route('/sign-up', methods=['GET', 'POST'])
     def signup():
         if request.method=='POST':
             user=request.form.get('user')
             password1=request.form.get('password1')
             password2=request.form.get('password2')
-            if len(user) < 3:
+            user=User.query.filter_by(user=user).first()
+            if user:
+                flash('Email already exist',category='error')
+
+            elif len(user) < 3:
                 flash('User must be greater then 3 characters',category='error')
             elif password1 != password2:
                 flash('Password must match',category='error')
@@ -53,6 +72,10 @@ with app.app_context():
                 flash('Password must be greater then 4 characters',category='error')
             else:
             #add user to database
+                new_user=User(user=user,password=generate_password_hash(passowrd1,method='sha256'))
                 flash('Account created!',category="success")
-
+                db.session.add(new_user)
+                db.session.commit()
+                login_user(user,remember=True)
+                return redirect(url_for('views.model'))
         return render_template("sign_up.html")
